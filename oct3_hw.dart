29761f1d400a11466358fbc3eb14b527542f4a4b
chloes0mt <114194943+chloes0mt@users.noreply.github.com>
2022-10-03 09:57:12 -0400

    Create oct3_hw.dart

---
+++ oct3_hw.dart
@@ -0,0 +1,10 @@
+getGreetingFunction(who) {
+  return who;
+}
+
+main () {
+  var greetingFunction = getGreetingFunction("Hello from C");
+  print(greetingFunction);
+}
+
+/* Output: Hello from C */
